e0ff9fb4f88f3ee664692707dbcfad3c311d9b72
facundo37858 <facundoomargarcia@gmail.com>
2022-09-05 17:24:14 -0300

    peticiones htpp

--- frontWithAngular/porfolioArgPro/src/app/app.module.ts
+++ frontWithAngular/porfolioArgPro/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { StackComponent } from './components/stack/stack.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
