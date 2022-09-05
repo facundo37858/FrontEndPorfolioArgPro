6cdc3488d3600ddd63cf682b3cad9d4da859fd59
facundo37858 <facundoomargarcia@gmail.com>
2022-09-05 14:49:30 -0300

    angular

---
+++ frontWithAngular/porfolioArgPro/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { StackComponent } from './components/stack/stack.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    StackComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
