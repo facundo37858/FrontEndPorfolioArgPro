d017fd02dbd6568632348316a6566a84efde4ae6
facundo37858 <facundoomargarcia@gmail.com>
2022-09-21 16:39:40 -0300

    login comp

--- frontWithAngular/porfolioArgPro/src/app/components/modal/modal.component.ts
+++ frontWithAngular/porfolioArgPro/src/app/components/modal/modal.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
   @Input() title=''
-  public show=false
+  public show=true
 
   constructor() { }
 
